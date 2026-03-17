812c07f6b199f73a2c65ecd046df7ed0fdc7dbe4
Oleg Khripushin <oleg19952009@yandex.ru>
2026-03-17 12:11:44 +0300

    task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let discriminant, arr = [];
+
+	discriminant = (b * b) - (4 * a * c);
+
+	if (discriminant > 0) {
+		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+	} else if (discriminant == 0) {
+		arr.push(-b / (2 * a));
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let S, P, monthAmount, totalAmount;
+
+	P = parseInt(percent) / 100 / 12;
+	S = amount - contribution;
+	monthAmount = S * (P + P / (((1 + P) ** countMonths) - 1));
+	totalAmount = monthAmount * countMonths;
+
+	return Number(totalAmount.toFixed(2));
 }
